5ba4e885251ae2ab1a8663cea059ddba3ecd10a6
roarrraor <rikkyarisendi@gmail.com>
2022-03-27 22:07:12 +0700

    upload files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+async function maubucin() {
+  await swals.fire({
+    title: "<strong>Ada hadiah buat kamu nih yang</strong>",
+    html: "Klik di " + '<a href="https:rikkyarisendi.github.io</a> ' + "ya!" + "<br><br>" + "Pokoknya wajib dibuka yang !.",
+    focusConfirm: false,
+  });
+}
